afc261a0189541809b1fcd5e7fa5de4bf6ab5e29
Sofia Torres <sofiavalentinatorrespicon@gmail.com>
2026-06-29 16:53:44 -0400

    feat: Update Firebase configuration with correct verified credentials

--- src/db/firebaseConfig.js
+++ src/db/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_gLaOBtysd0Y0jGvVIC8fCyKn8Egk7-c",
+  apiKey: "AIzaSyB_glAoBTysdYOyJGvVIC0fCyKmBEgk7-c",
   authDomain: "entre-tazas.firebaseapp.com",
   projectId: "entre-tazas",
   storageBucket: "entre-tazas.firebasestorage.app",
   messagingSenderId: "372132177449",
-  appId: "1:372132177449:web:1384b4de0054444348de38",
+  appId: "1:372132177449:web:1384b4de0005444348de38",
   measurementId: "G-GFHV8LNP4Z"
 };
 
